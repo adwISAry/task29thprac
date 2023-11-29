01006632c92f5bf22fc314360228da0771f022c5
Isa Aliev <146819571+adwISAry@users.noreply.github.com>
2023-11-29 13:33:26 +0400

    Add files via upload

---
+++ practask29th/practask29th/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practask29th.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
